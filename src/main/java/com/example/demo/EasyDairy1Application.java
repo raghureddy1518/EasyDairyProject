bfe0c5a36a042f769a5a84ea48bb0da3b71b18d8
raghu <kurmeraghureddy6@gmail.com>
2025-08-10 17:47:58 +0530

    new chnges

--- src/main/java/com/example/demo/EasyDairy1Application.java
+++ src/main/java/com/example/demo/EasyDairy1Application.java
@@ -8,13 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EasyDairy1Application {
 
     public static void main(String[] args) {
-        // Load environment variables from .env file
-        Dotenv dotenv = Dotenv.load();
-        
-        // Set them as System properties so Spring can use ${VAR_NAME}
-        dotenv.entries().forEach(entry -> 
-            System.setProperty(entry.getKey(), entry.getValue())
-        );
+
+        // Check if running locally (Render sets "RENDER" env variable in production)
+        if (System.getenv("RENDER") == null) {
+            // Load environment variables from .env file (local only)
+            Dotenv dotenv = Dotenv.load();
+
+            // Set them as System properties so Spring can use ${VAR_NAME}
+            dotenv.entries().forEach(entry ->
+                    System.setProperty(entry.getKey(), entry.getValue())
+            );
+        }
 
         SpringApplication.run(EasyDairy1Application.class, args);
     }
